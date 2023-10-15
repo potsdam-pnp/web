647e14e766db6e16d21705fb1d5a79ae9ef68ddc
Fabian Schmitthenner <development@schmitthenner.eu>
2023-10-15 16:03:27 +0200

    cont

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -6,27 +6,30 @@ const cardImages = import.meta.glob("../../dependencies/card-images/*.png");
 
 const cardContent = document.getElementById('card-content');
 
-for (const key in cardMetadata) {
-  const {title, level, type} = cardMetadata[key];
+async function addImages() {
+  for (const key in cardMetadata) {
+    const {title, level, type} = cardMetadata[key];
 
-  if (level < 0) continue;
+    if (level < 0) continue;
 
-  const imageUrl = await cardImages["../../dependencies/card-images/" + key + ".png"]();
-  const thisCard = document.createElement("div");
-  thisCard.classList.add('col');
-  // <img src="..." class="card-img-top" alt="...">
-  thisCard.innerHTML = `
-    <div class="card">
-    <img src="${imageUrl.default}" class="card-img-top" alt="Card image">
-    <div class="card-body">
-      <h5 class="card-title">${title}</h5>
-      <p class="card-subtitle mb-2 text-body-secondary">${type} ${level}</p>
-    </div>
-    </div>
-  `;
-  cardContent.appendChild(thisCard);
+    const imageUrl = await cardImages["../../dependencies/card-images/" + key + ".png"]();
+    const thisCard = document.createElement("div");
+    thisCard.classList.add('col');
+    // <img src="..." class="card-img-top" alt="...">
+    thisCard.innerHTML = `
+      <div class="card">
+      <img src="${imageUrl.default}" class="card-img-top" alt="Card image">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-subtitle mb-2 text-body-secondary">${type} ${level}</p>
+      </div>
+      </div>
+    `;
+    cardContent.appendChild(thisCard);
+  }
 }
-
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+addImages();
