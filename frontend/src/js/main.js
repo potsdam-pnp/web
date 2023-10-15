38a91fa74f977a3d169e9e47cd481f8e71e20098
Fabian Schmitthenner <development@schmitthenner.eu>
2023-10-15 16:01:08 +0200

    Show all the cards

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -2,10 +2,31 @@
 import '../scss/styles.scss'
 
 import cardMetadata from '../../dependencies/card-metadata.json';
-
 const cardImages = import.meta.glob("../../dependencies/card-images/*.png");
 
-console.log(cardMetadata, cardImages);
+const cardContent = document.getElementById('card-content');
+
+for (const key in cardMetadata) {
+  const {title, level, type} = cardMetadata[key];
+
+  if (level < 0) continue;
+
+  const imageUrl = await cardImages["../../dependencies/card-images/" + key + ".png"]();
+  const thisCard = document.createElement("div");
+  thisCard.classList.add('col');
+  // <img src="..." class="card-img-top" alt="...">
+  thisCard.innerHTML = `
+    <div class="card">
+    <img src="${imageUrl.default}" class="card-img-top" alt="Card image">
+    <div class="card-body">
+      <h5 class="card-title">${title}</h5>
+      <p class="card-subtitle mb-2 text-body-secondary">${type} ${level}</p>
+    </div>
+    </div>
+  `;
+  cardContent.appendChild(thisCard);
+}
+
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
