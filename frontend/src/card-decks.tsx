9fec8bb2af2e9220289f71d376d3468e21271cb0
Fabian Schmitthenner <development@schmitthenner.eu>
2023-10-22 22:22:53 +0000

    Add "add card deck" modal

--- frontend/src/card-decks.tsx
+++ frontend/src/card-decks.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { CardProperty } from "./card";
-
+import { Modal } from "bootstrap";
 
 interface CardDeck {
   name: string;
@@ -33,38 +33,44 @@ class CardDecksPicker extends React.Component<CardDecksProps> {
     );
     const addDeck = React.createElement("button", { 
       type: "button", 
-      className: "btn btn-outline-secondary"
+      className: "btn btn-outline-secondary",
+      //"bs-data-toggle": "modal",
+      //"bs-data-target": "#add-card-deck-modal"
+      onClick: () => (new Modal("#add-card-deck-modal", {  })).show()
     }, 
       "Create new deck"
     );
 
     return React.createElement("div", { className: "btn-group", role: "group" },
+      React.createElement(AddCardDeckModal, {}),
       ...children,
       addDeck);
   }
 }
 
 
-// class AddCardDeckModal extends React.Component {
-//   render() {
-//     return (
-//       <div id="add-card-deck-modal" className="modal fade" aria-hidden="true" tabIndex={-1}>
-//         <div className="modal-dialog">
-//           <div className="modal-content">
-//             <div className="modal-header">
-//               <h5 className="modal-title">Modal title</h5>
-//               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//             </div>
-//             <div className="modal-body">
-//               <p>Modal body text goes here.</p>
-//             </div>
-//             <div className="modal-footer">
-//               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//               <button type="button" className="btn btn-primary">Save changes</button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+class AddCardDeckModal extends React.Component {
+  render() {
+    return (
+      <div id="add-card-deck-modal" className="modal fade" aria-hidden="true" tabIndex={-1}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Add new card deck</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <div className="form-group"></div>
+                <label htmlFor="recipient-name" className="col-form-label">Card deck name:</label>
+                <input type="text" className="form-control" id="recipient-name"></input>
+              </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Create</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
