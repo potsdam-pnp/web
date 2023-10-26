2c8225af98d4f68c874dab3f0e287c528c4fa921
Fabian Schmitthenner <development@schmitthenner.eu>
2023-10-26 16:52:00 +0000

    fix bug that card is not re-rendered

--- frontend/src/views/show-card.tsx
+++ frontend/src/views/show-card.tsx
@@ -4,14 +4,18 @@ import { Card } from "../lib/card";
 let cardSources: Record<string, string> = {};
 const cardImages = (import.meta as any).glob("../../dependencies/card-images/*.png");
 
-
 export function ShowCard({card, children}: {card: Card, children: ReactNode}) {
-  const [src, setSrc] = useState(cardSources[card.page]);
+  const [src, setSrc] = useState({ [card.page]: cardSources[card.page] });
 
-  if (src) {
+  if (!(card.page in src)) {
+    setSrc({ [card.page]: cardSources[card.page] });
+    return;
+  }
+
+  if (src[card.page]) {
     const { title, type, level } = card;
     return createElement("div", {className: "card"},
-        createElement("img", { src: src, className: "card-img-top", alt: "Card image"}),
+        createElement("img", { src: src[card.page], className: "card-img-top", alt: "Card image"}),
         createElement("div", { className: "card-body d-flex flex-row justify-content-between"},
           createElement("div", {},
             createElement("h5", { className: "card-title" }, title),
@@ -26,7 +30,7 @@ export function ShowCard({card, children}: {card: Card, children: ReactNode}) {
     const page = card.page;
     cardImages["../../dependencies/card-images/" + page + ".png"]().then((result: any) => {
       cardSources[page] = result.default;
-      setSrc(result.default);
+      setSrc({ [card.page]: result.default });
     });
     return <>"Loading"</>;
   }
