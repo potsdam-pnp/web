c0ca19fabe73ced539f929154002cb08d745b7bb
Fabian Schmitthenner <development@schmitthenner.eu>
2023-10-28 22:02:12 +0000

    Eagerly load card images
    It makes no sense to lazily load a javascript that just contains a small string

--- frontend/src/views/show-card.tsx
+++ frontend/src/views/show-card.tsx
@@ -2,28 +2,13 @@ import { ReactNode, useState } from "react";
 import { Card as BCard } from "react-bootstrap";
 import { Card } from "../lib/card";
 
-let cardSources: Record<string, string> = {};
-const cardImages = (import.meta as any).glob("../../dependencies/card-images/*.png");
+const cardImages = (import.meta as any).glob("../../dependencies/card-images/*.png", { eager: true });
 
 export function ShowCard({card, children}: {card: Card, children: ReactNode}) {
-  const [src, setSrc] = useState({ [card.page]: cardSources[card.page] });
-
-  if (!(card.page in src)) {
-    setSrc({ [card.page]: cardSources[card.page] });
-    return;
-  }
-
-  if (!src[card.page]) {
-    const page = card.page;
-    cardImages["../../dependencies/card-images/" + page + ".png"]().then((result: any) => {
-      cardSources[page] = result.default;
-      setSrc({ [card.page]: result.default });
-    });
-  }
-
-  const { title, type, level } = card;
+  const { title, type, level, page } = card;
+  const src = cardImages["../../dependencies/card-images/" + page + ".png"].default;
   return <BCard>
-    <BCard.Img variant="top" src={src[card.page] || ""} />
+    <BCard.Img variant="top" src={src} />
     <BCard.Body className="d-flex flex-row justify-content-between">
       <div>
         <BCard.Title>{title}</BCard.Title>
