53706ac2ce41ca8ce64fb14ce4a3109271757e01
Fabian Schmitthenner <development@schmitthenner.eu>
2023-10-30 11:50:30 +0000

    Add flake to generate pdf

---
+++ generate-pdf/default.nix
@@ -0,0 +1,29 @@
+{ texlive, stdenv, lib, runCommand, cards, pages }:
+
+stdenv.mkDerivation {
+  name = "cards.pdf";
+  nativeBuildInputs = [(texlive.combine { inherit (texlive) scheme-small latexmk; })];
+
+  src = runCommand "our-cards.tex" { 
+    inherit pages;
+  } ''
+    echo "\documentclass[paper=a4]{scrartcl}" > $out
+    echo "\usepackage{pdfpages}" >> $out
+    echo "\begin{document}" >> $out
+    echo "\includepdf[pages={$(cat $pages)},nup=3x3,noautoscale,frame]{cards}" >> $out
+    echo "\end{document}" >> $out
+  '';
+
+  unpackPhase = ''
+    cp $src our-cards.tex
+    ln -s ${cards}/cards.pdf .
+  '';
+
+  buildPhase = ''
+    latexmk -xelatex our-cards.tex
+  '';
+
+  installPhase = ''
+    cp our-cards.pdf $out
+  '';
+}
---
+++ generate-pdf/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "generate-pdf";
+    inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+      flake-utils.url = "github:numtide/flake-utils";
+      pf2e-cards.url = "github:potsdam-pnp/pf2e-cards";
+      pages = {
+        flake = false;
+        type = "file";
+        url = "https://raw.githubusercontent.com/potsdam-pnp/web/main/frontend/generate-pdf/pages";
+      };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pf2e-cards, pages }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+          cards = pf2e-cards.packages.${system}.cards; in {
+      packages.default = pkgs.callPackage ./. { inherit cards pages; };
+    });
+}
